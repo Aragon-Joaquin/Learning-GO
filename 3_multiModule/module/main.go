68584169a0d547f24bfd3a76e2b8055d7dff3eb8
unknown <aragonjoaquin05@gmail.com>
2025-01-29 18:03:01 -0300

    added explanation to 3_multiModule

--- 3_multiModule/module/main.go
+++ 3_multiModule/module/main.go
@@ -8,4 +8,5 @@ import (
 
 func main() {
 	fmt.Println(reverse.String("This string will be reversed"), reverse.Int(123))
+	// reverse.Int() is not a native function from the example/hello/reverse package, the Go tutorial helps you implement it by modifying the reverse.go package
 }
